8a7cf67dafc487e828e1da785e43301fcf756629
Andre Ibarra Perez <RayAsm1@hotmail.com>
2020-02-05 20:13:05 -0600

    V1

---
+++ ClimaMethod3/Ciudad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClimaMethod3
+{
+    class Ciudad
+    {
+        public string CityId { get; set; }
+        public string Name { get; set; }
+        public string StateAbbr { get; set; }
+        public string HourNumber { get; set; }
+        public string LastReportTime { get; set; }
+        public string DayOfWeek { get; set; }
+        public string ValidDateUtc { get; set; }
+        public string LocalValidDate { get; set; }
+        public string TempF { get; set; }
+        public string TempC { get; set; }
+        public string SkyDescriptionLong { get; set; }
+        public string ProbabilityOfPrecip { get; set; }
+        public string RelativeHumidity { get; set; }
+        public string WindSpeedMph { get; set; }
+        public string WindSpeedKm { get; set; }
+        public string WindDirectionCardinal { get; set; }
+        public string IconCode { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
---
+++ ClimaMethod3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO.Compression;
+using System.IO;
+using Newtonsoft.Json;
+using System.Runtime.Serialization.Json;
+using ClimaMethod3;
+using System.Globalization;
+
+namespace Clima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetRequest("https://smn.cna.gob.mx/webservices/index.php?method=3");
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+
+        async static void GetRequest(String url)
+        {
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(url)) //obtener una variable con la info del url
+            using (var content = await response.Content.ReadAsStreamAsync()) //obtener la info del archivo
+            using (var descomprimido = new GZipStream(content, CompressionMode.Decompress)) //descomprimir el archivo
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    StreamReader reader = new StreamReader(descomprimido);
+                    String data = reader.ReadLine();
+                    var listInfo = JsonConvert.DeserializeObject<List<Ciudad>>(data);
+                    foreach (var info in listInfo)
+                    {
+                        if (info.CityId.Equals("MXTS2043"))
+                        {
+                            Console.WriteLine("ID: " + info.CityId + "\nNombre: " + info.Name + "\nAbreviatura: " + info.StateAbbr + "\nNumero de hora: " + info.HourNumber
+                                + "\nUltimo reporte: " + DateTime.ParseExact(info.LastReportTime.Substring(0, 8), "yyyyMMdd",CultureInfo.InvariantCulture).ToLongDateString() + "\nDía de la semana: " + info.DayOfWeek + "\nFechaUTC: " + DateTime.ParseExact(info.ValidDateUtc.Substring(0, 8), "yyyyMMdd", CultureInfo.InvariantCulture).ToLongDateString()
+                                + "\nFecha Local: " + DateTime.ParseExact(info.LocalValidDate.Substring(0, 8), "yyyyMMdd", CultureInfo.InvariantCulture).ToLongDateString() + "\nTemperatura: " + info.TempC + "\nDescripción del cielo: " + info.SkyDescriptionLong
+                                + "\nProbabilidad de precipitación: " + info.ProbabilityOfPrecip + "\nHumedad Relativa: " + info.RelativeHumidity
+                                + "\nVelocidad del viento en mph: " + info.WindSpeedMph + "\nVelocidad del viento en Km: " + info.WindSpeedKm
+                                + "\nDirección del viento cardenal: " + info.WindDirectionCardinal + "\nCódigo del icono: " + info.IconCode + "\nLatitud: " + info.Latitude + "\nLongitud: " + info.Longitude +"\n");
+                        }
+                    }
+
+                }
+
+
+            }
+
+
+            /*{
+                String data = await content.ReadAsStringAsync();
+                if(data != null)
+                {
+                    Console.WriteLine(data);
+                }
+            }*/
+        }
+    }
+}
